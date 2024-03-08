a5bb368764edbf191bc469d2517a9381d5a6271e
Harrison <harrison.pinto@d4d.com.co>
2024-03-08 16:33:37 -0500

    Invoke __call__ on boolean checks

--- checks/BaseCheck.py
+++ checks/BaseCheck.py
@@ -11,6 +11,9 @@ class BaseCheck(abc.ABC):
     def passes(self) -> tuple[bool, str]:
         return self(), 'Incumple' if not self() else 'Cumple'
 
+    def __bool__(self) -> bool:
+        return self()
+
     def __str__(self) -> str:
         _, message = self.passes()
         return f'{message} {self.check}'
