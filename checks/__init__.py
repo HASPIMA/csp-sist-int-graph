99490319b97606491a4e9b460f842694c2e832fc
Harrison <harrison.pinto@d4d.com.co>
2024-03-08 16:26:55 -0500

    Add TODO

--- checks/__init__.py
+++ checks/__init__.py
@@ -3,6 +3,7 @@ from .o_check import o_div, o_remainder
 from .w_check import w_div, w_remainder
 from .fo_check import t_div, t_remainder, carry_check
 
+# TODO: Rename this module to constraints
 
 __all__ = [
     'alldifferent',
