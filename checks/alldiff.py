5a82ebad921e41075757defbaa231be0094c35d5
Harrison <harrison.pinto@d4d.com.co>
2024-03-08 17:31:26 -0500

    Fix alldiff to handle None values

--- checks/alldiff.py
+++ checks/alldiff.py
@@ -3,13 +3,13 @@ from .BaseCheck import BaseCheck
 
 class alldifferent(BaseCheck):
     check = 'alldiff'
-    this: dict[str, int]
+    this: dict[str, int | None]
 
-    def __init__(self, **kwargs: int):
+    def __init__(self, **kwargs: int | None):
         self.this = kwargs
 
     def __call__(self,) -> bool:
-        vals = self.this.values()
+        vals = [v for v in self.this.values() if v is not None]
         return len(set(vals)) == len(vals)
 
     def __str__(self):
